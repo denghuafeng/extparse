c6c3ed3a46c88393343d5afd038ba9fce6493a3d
amonsoft <amonsoft@e59e9dac-6122-11de-8832-3f4c773c73ae>
2009-07-01 14:17:53 +0000

    后缀解析，项目初始化。

---
+++ trunk/ext/ext/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Resources;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("后缀解析")]
+[assembly: AssemblyDescription("后缀解析，为您提供更为详细的文件扩展名信息！")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Winshine")]
+[assembly: AssemblyProduct("Extparse")]
+[assembly: AssemblyCopyright("Copyright © Winshine 2009")]
+[assembly: AssemblyTrademark("Amonsoft")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2e0e8973-99de-4272-95cc-af5615c4e7a1")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("3.3.0.0")]
+[assembly: AssemblyFileVersion("3.3.0.0")]
+[assembly: NeutralResourcesLanguageAttribute("zh-CHS")]
