4117dd5a263f661af9b53b6c1475670d6781d26d
amonsoft <amonsoft@e59e9dac-6122-11de-8832-3f4c773c73ae>
2009-07-01 14:17:53 +0000

    后缀解析，项目初始化。

---
+++ ext/ext/Program.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Windows.Forms;
+
+namespace com.amonsoft.extparse
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new FM_ExtParse());
+        }
+    }
+}
---
+++ ext/src/com/amonsoft/extparse/Extparse.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.amonsoft.extparse;
+
+/**
+ *
+ * @author Amon
+ */
+public class Extparse {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
