f65aa7700473bae6cce944a6d21ba127abad24ec
amonsoft <amonsoft@e59e9dac-6122-11de-8832-3f4c773c73ae>
2010-06-30 06:25:33 +0000

    后缀解析添加截图功能。

---
+++ ext/ext/RECT.cs
@@ -0,0 +1,10 @@
+﻿namespace com.amonsoft.extparse
+{
+    internal struct RECT  //Win32汇编数据结构不用自己定义
+    {
+        public int left;
+        public int top;
+        public int right;
+        public int bottom;
+    }
+}
